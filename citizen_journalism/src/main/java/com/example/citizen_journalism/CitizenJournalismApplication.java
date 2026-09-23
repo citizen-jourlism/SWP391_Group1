4b52451b5d8d4872d4d15afab1b4b7294bd82361
Binh2k5 <154050227+Binh2k5@users.noreply.github.com>
2026-09-23 23:04:24 +0700

    initial backend codebase

---
+++ citizen_journalism/src/main/java/com/example/citizen_journalism/CitizenJournalismApplication.java
@@ -0,0 +1,13 @@
+package com.example.citizen_journalism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenJournalismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenJournalismApplication.class, args);
+	}
+
+}
---
+++ citizen_journalism/src/test/java/com/example/citizen_journalism/CitizenJournalismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citizen_journalism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenJournalismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
